1955593b3508c7d4494e4bcacc81e576f1437a3d
Adecoded <adebisidrey@gmail.com>
2022-04-09 22:22:28 +0100

    new login

--- App.js
+++ App.js
@@ -1,17 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity, ScrollView} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <ScrollView>
+      <View>
+        <View style={{marginTop:60, alignItems:"center", justifyContent:"center"}}>
+          <Image source={require('./assets/logo.png')}></Image>
+          <Text>HoneyWealth</Text>
+        </View>
+        <View>
+          <TouchableOpacity>
+            <View>
+            <Image source={require('./assets/facebook.png')}></Image>
+            <Text>Facebook</Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <View>
+            <Image source={require('./assets/google.png')}></Image>
+            <Text>Google</Text>
+            </View>
+          </TouchableOpacity>
+          <Text>or</Text>
+
+          <Text>Forget Password?</Text>
+
+          <TouchableOpacity>
+            <Text>Login</Text>
+          </TouchableOpacity>
+
+          <Text>Don't have account? <Text>Register now</Text></Text>
+        </View>
+      </View>
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  text: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
